0c68f55a72cbba0153995c56f3fb35401cebb3dd
M64GitHub <mario.a.schallner@gmail.com>
2023-08-03 18:32:38 +0200

    add reSID files

--- zsp_sid.c
+++ zsp_sid.c
@@ -1,5 +1,20 @@
 #include "zsp_sid.h"
 
+#include "reSID/envelope.cc"
+#include "reSID/extfilt.cc"
+#include "reSID/filter.cc"
+#include "reSID/pot.cc"
+#include "reSID/version.cc"
+#include "reSID/voice.cc"
+
+#include "reSID/wave8580__ST.cc" 
+#include "reSID/wave8580_P_T.cc"
+#include "reSID/wave8580_PS_.cc"
+#include "reSID/wave8580_PST.cc"
+
+#include "reSID/wave.cc"
+#include "reSID/sid.cc"
+
 CHANNEL chn[3];
 CHANNEL prevchn[3];
 CHANNEL prevchn2[3];
